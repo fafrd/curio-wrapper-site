7c1cf389568574ee7976a06cb1d2053abb1cd2e2
Kian <kian@kian>
2021-03-23 23:37:40 -0700

    figure out erc1155 balances, set up <main>, set up image onclicks

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 import { ethers } from "ethers";
 import 'regenerator-runtime/runtime'
-import * as constants from "./constants.js"
+//import * as constants from "./constants.js"
+import * as constants from "./rinkeby-constants.js"
+
+// Override the owner address. Used instead of metamask address if set
+//const debugAddress = "0x3be11d51f5b1353b305eaf926376ed437634c3dc";
+//const debugAddress = "0x85a9d6258b6a2cc264fdbfb60c5e3d2678b4ef7e";
+const debugAddress = "0x49468f702436d1e590895ffa7155bcd393ce52ae";
+
+const numCards = 5; // debug
 
 const { ethereum } = window;
 if (ethereum) {
@@ -25,7 +33,7 @@ async function connectWallet() {
     }
 
     // once connected, perform the next tasks, like populating token balances
-    postConnection(accts[0]);
+    postConnection(debugAddress || accts[0]);
 }
 
 async function getErc20Balance(tokenAddr, userAddr) {
@@ -33,14 +41,35 @@ async function getErc20Balance(tokenAddr, userAddr) {
     //await new Promise(resolve => setTimeout(resolve, Math.random() * 500));
 
     const contract = await new ethers.Contract(tokenAddr, constants.erc20Abi, provider);
-    const balance = await contract.balanceOf(userAddr);
-    //console.log("balance: " + balance);
-    return balance;
+    return await contract.balanceOf(userAddr);
 }
 
-async function getErc1155BalanceBatch(contractAddr, tokenAddrs, userAddr) {
+async function getErc1155BalanceBatch(contractAddr, userAddr) {
     const contract = await new ethers.Contract(contractAddr, constants.wrapperAbi, provider);
-    const balances = await contract.balanceOfBatch([userAddr], tokenAddrs);
+    console.log("querying batch... ");
+    console.log(contract["balanceOfBatch(address[],uint256[])"]);
+    const userAddrArray = Array(30).fill(userAddr);
+    let idArray = Array.from(Array(31).keys());
+    idArray.shift();
+
+    return await contract.balanceOfBatch(userAddrArray, idArray);
+}
+
+async function handleCardClick(event) {
+    let currentCard, currentType;
+    if (event.path[1].id.startsWith("nav-card-erc20")) {
+        currentCard = Number(event.path[1].id.substring(15));
+        currentType = "erc20";
+    } else if (event.path[1].id.startsWith("nav-card-erc1155")) {
+        currentCard = Number(event.path[1].id.substring(17));
+        currentType = "erc1155";
+    }
+
+    console.log("handleCardClick: " + currentType + " #" + currentCard);
+
+    // TODO- set Selected state.
+    //     this means hide/unhide table row,
+    //     and overlay the card nav image as "selected" or not
 }
 
 // work to perform after connection is established
@@ -49,18 +78,34 @@ async function postConnection(userAddr) {
     document.getElementById("web3").innerText = "connected as " + userAddr.substring(0, 5) + "…" + userAddr.substring(userAddr.length-3);
 
     // populate erc20 balances
-    for (let i = 1; i < 30; i++) {
+    for (let i = 1; i <= numCards; i++) {
         const currentSymbol = "CRO" + i;
         getErc20Balance(constants.curioAddresses[currentSymbol], userAddr).then(balance => {
             console.log("erc20 " + currentSymbol + " balance: " + balance);
             document.getElementById("nav-card-erc20-" + i + "-balance").innerText = balance;
+            document.getElementById("main-card-erc20-" + i + "-balance").innerText = balance;
         });
     }
 
     // populate erc1155 balances
-//    getErc1155BalanceBatch(constants.wrapperAddr, Object.values(constants.curioAddresses), userAddr).then(balances => {
-//        console.log("erc1155 balances: " + balances);
-//    });
+    getErc1155BalanceBatch(constants.wrapperAddr, userAddr).then(balances => {
+        console.log("erc1155 balances: " + balances);
+        for (let i = 1; i <= numCards; i++) {
+            console.log("erc1155 id " + i + " balance: " + balances[i-1]);
+            document.getElementById("nav-card-erc1155-" + i + "-balance").innerText = balances[i-1];
+            document.getElementById("main-card-erc1155-" + i + "-balance").innerText = balances[i-1];
+        }
+    });
+
+    // bind event listeners to nav cards
+    for (let i = 1; i <= numCards; i++) {
+        const erc20Element = document.getElementById("nav-card-erc20-" + i);
+        erc20Element.addEventListener("click", handleCardClick);
+        erc20Element.classList.add("pointer");
+        const erc1155Element = document.getElementById("nav-card-erc1155-" + i);
+        erc1155Element.addEventListener("click", handleCardClick);
+        erc1155Element.classList.add("pointer");
+    }
 }
 
 async function initialize() {
@@ -70,8 +115,8 @@ async function initialize() {
     // detect if metamask is already connected; set button in top right
     provider.listAccounts().then(accts => {
         if (accts.length > 0) {
-            console.log("connected! " + JSON.stringify(accts));
-            postConnection(accts[0]);
+            console.log("connected! " + debugAddress || JSON.stringify(accts));
+            postConnection(debugAddress || accts[0]);
         } else {
             console.log("not yet connected!");
             document.getElementById("web3").addEventListener("click", connectWallet);
